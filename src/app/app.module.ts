4e73db06ecfc2e5bbf4be94c2d9337fc930dc9f0
Kostiantyn Solohub <kostiantyn.solohub@amdaris.com>
2022-07-05 17:44:54 +0300

    create home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HomeModule } from './home/home.module';
 
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
