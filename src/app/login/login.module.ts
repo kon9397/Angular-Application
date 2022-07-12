27326bd3aff94e87dc3aa81019f0f2388333f4c0
Kostiantyn Solohub <kostiantyn.solohub@amdaris.com>
2022-07-12 18:51:17 +0300

    add lazy-loading, create admin dashboard module

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,18 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login.component';
-import { RouterModule } from '@angular/router';
-import { loginRoutes } from './login.route';
-import { ReactiveFormsModule } from '@angular/forms';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LoginComponent } from "./login.component";
+import { RouterModule } from "@angular/router";
+import { loginRoutes } from "./login.route";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
-  declarations: [LoginComponent],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(loginRoutes),
-    ReactiveFormsModule
-  ]
+    declarations: [LoginComponent],
+    imports: [CommonModule, RouterModule.forChild(loginRoutes), SharedModule],
 })
 export class LoginModule { }
